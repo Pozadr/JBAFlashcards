727bd8409a2f2cd9693a55bc703bbc3fdef18960
Pozadr <adpozor@gmail.com>
2020-10-13 19:25:11 +0200

    Stage 4/7: A good stack -> done

--- Flashcards/task/src/flashcards/Flashcards.java
+++ Flashcards/task/src/flashcards/Flashcards.java
@@ -1,10 +1,9 @@
 package flashcards;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Flashcards {
-    private Map<String, String> flashcards = new HashMap<>();
+    private final Map<String, String> flashcards = new LinkedHashMap<>();
 
     public void printFlashcards() {
         flashcards.forEach((term, definition) -> System.out.println(term + ": " + definition));
@@ -20,5 +19,27 @@ public class Flashcards {
 
     public boolean isDefinitionExist(String definition) {
         return flashcards.containsValue(definition);
+    }
+
+    public String getTermToDefinition(String definition) {
+        if (isDefinitionExist(definition)) {
+            for (String term : getFlashcardsTerms()) {
+                if (flashcards.get(term).equals(definition)) {
+                    return term;
+                }
+            }
+        }
+        return "Definition not exists in flashcards.";
+
+    }
+
+    public Set<String> getFlashcardsTerms() {
+        return new LinkedHashSet<>(flashcards.keySet());
+    }
+    public Set<String> getFlashcardsDefinitions() {
+        return new HashSet<>(flashcards.values());
+    }
+    public String getFlashcardDefinition(String key) {
+        return flashcards.get(key);
     }
 }
--- Flashcards/task/src/flashcards/Main.java
+++ Flashcards/task/src/flashcards/Main.java
@@ -1,19 +1,17 @@
 package flashcards;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Flashcards flashcards = new Flashcards();
 
-        // ---------- UI ----------
+        // Get flashcards
         System.out.println("Input the number of cards:");
         int howManyCards = 0;
         try {
             howManyCards = Integer.parseInt(scanner.nextLine().trim());
-
-
         } catch (NumberFormatException e) {
             System.out.println("You need to type numbers!");
         }
@@ -28,25 +26,40 @@ public class Main {
                 term = scanner.nextLine().trim();
                 finishLoop = flashcards.isTermExist(term);
                 if (finishLoop) {
-                    System.out.println("The term " + term + " already exists. Try again:");
+                    System.out.println("The card \"" + term + "\" already exists. Try again:");
                 }
             }
 
-            System.out.println("The definition for card #1:");
+            System.out.println("The definition for card #" + (i + 1) + ":");
             finishLoop = true;
             while (finishLoop) {
                 definition = scanner.nextLine().trim();
                 finishLoop = flashcards.isDefinitionExist(definition);
                 if (finishLoop) {
-                    System.out.println("The definition " + definition + " already exists. Try again:");
+                    System.out.println("The definition \"" + definition + "\" already exists. Try again:");
                 }
             }
 
             flashcards.addFlashcard(term, definition);
         }
 
+        // Get answers
+        for (String term : flashcards.getFlashcardsTerms()) {
+            System.out.println("Print the definition of \"" + term + "\":");
+            String userDefinition = scanner.nextLine().trim();
+            if (userDefinition.equals(flashcards.getFlashcardDefinition(term))) {
+                System.out.println("Correct!");
+            } else if (flashcards.isDefinitionExist(userDefinition)){
+                String goodTerm = flashcards.getTermToDefinition(userDefinition);
+                System.out.println("Wrong. The right answer is \""+
+                        flashcards.getFlashcardDefinition(term)+ "\"" +
+                        ", but your definition is correct for \"" + goodTerm + "\".");
+            } else {
+                System.out.println("Wrong. The right answer is \""+
+                        flashcards.getFlashcardDefinition(term)+ "\"");
+            }
+        }
 
-        flashcards.printFlashcards();
 
     }
 }
